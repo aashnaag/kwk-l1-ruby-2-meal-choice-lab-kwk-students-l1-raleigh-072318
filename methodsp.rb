c85ad37e16075cfc6ced5826b5ec0ff696d523d4
aashnaag <aashna.agarwal55@gmail.com>
2018-07-24 14:23:19 +0000

    automatically backed up by learn

--- methodsp.rb
+++ methodsp.rb
@@ -6,9 +6,12 @@ height = "62inches"
 
 puts "What is your name?"
 my_name = gets
+
 puts "What is your birth_date?"
 birth_date = gets
+
 puts "Where were you born?"
 birth_place = gets
+
 puts "How tall are you?"
 height=gets
