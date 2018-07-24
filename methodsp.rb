143fc47aa6d532b5119c348691269e8359127d6a
aashnaag <aashna.agarwal55@gmail.com>
2018-07-24 14:19:23 +0000

    automatically backed up by learn

---
+++ methodsp.rb
@@ -0,0 +1,15 @@
+my_name = "Aashna Agarwal"
+birth_date = "October10th"
+birth_place = "Raleigh,NC"
+height = "62inches"
+
+puts my_name + "was born in" + "and is from" + "and is"
+
+puts "What is your name?"
+my_name = gets
+puts "What is your birth_date"
+birth_date = gets
+puts "Where were you born?"
+birth_place = gets
+puts "How tall are you?"
+height=gets
