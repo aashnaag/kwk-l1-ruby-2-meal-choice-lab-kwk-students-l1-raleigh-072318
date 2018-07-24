3808e333b6f372f2ec54a821263b64617ed2c66d
aashnaag <aashna.agarwal55@gmail.com>
2018-07-24 14:49:00 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -10,15 +10,15 @@ lunch_type = "grilled cheese"
 dinner_type = "salmon"
 
 def breakfast(breakfast_type=frosted flake)
-  puts "Morning is the best time for #{breakfast_type}"
+  puts "Morning is the best time for #{breakfast_type}."
 end
 
 def lunch(lunch_type=grilled cheese)
-  puts "Afternoon is the best time for #{lunch_type}"
+  puts "Afternoon is the best time for #{lunch_type}."
 end
 
 def dinner(dinner_type=salmon)
-  puts "Night is the best time for #{dinner_type}"
+  puts "Night is the best time for #{dinner_type}."
 end
   
 
